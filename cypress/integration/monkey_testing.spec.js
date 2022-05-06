eaa8c3ffda34890d34c05801b301418db265d5bb
anfedelahoz <andelahozg@gmail.com>
2022-05-05 22:42:32 -0500

    NewTestingFeature: Create post with video and access for only members

--- cypress/integration/monkey_testing.spec.js
+++ cypress/integration/monkey_testing.spec.js
@@ -1,7 +1,7 @@
 import LoginPage from "../support/loginPage";
 import Dashboard from "../support/dashboardPage";
 import PostsPage from "../support/postsPage";
-import SinglePostPage from "../support/singlePostPage";
+import CreatePostPage from "../support/createPostPage";
 
 describe("Prueba Monkey sobre Ghost App", function () {
   it("Iniciar sesion como administrador", function () {
@@ -11,7 +11,6 @@ describe("Prueba Monkey sobre Ghost App", function () {
     loginPage.getEmail().type("andelahozg@gmail.com");
     loginPage.getPassword().type("Centro2021#");
     loginPage.getLoginButton().click();
-
     cy.log("Escenario con Usuario NO registrado");
     cy.visit("http://localhost:2368/ghost/");
     loginPage.getEmail().type("desconocido@gmail.com");
@@ -21,154 +20,179 @@ describe("Prueba Monkey sobre Ghost App", function () {
 
   it("Crear post", function () {
     login();
-
-    /* ==== Create post function ==== */
+    // /* ==== Create post function ==== */
     const dashboardPage = new Dashboard();
     const postPage = new PostsPage();
-    const singlePostPage = new SinglePostPage();
-
-    cy.log("Escenario con datos validos");
+    const createPostPage = new CreatePostPage();
+    cy.wait(3500);
+    cy.log("Escenario con preview");
     dashboardPage.getPostsMenu().click({ timeOut: 6000 });
     cy.wait(2500);
     postPage.getNewPostButton().click({ timeOut: 6000 });
-    singlePostPage.getPostTitle().type("A post for testing with Cypress");
-    singlePostPage.getPostContent().type("Hi there, now we are just testing.");
-    singlePostPage.getFirstPublishButton().click({ timeOut: 6000 });
-    singlePostPage.getSecondPublishButton().click({ timeOut: 6000 });
-    singlePostPage.getThirdPublishButton().click({ timeOut: 6000 });
+    createPostPage.getPostTitle().type("A post for testing with Cypress");
+    createPostPage.getPostContent().type("Hi there, now we are just testing.");
+    createPostPage.getFirstPublishButton().click({ timeOut: 6000 });
+    createPostPage.getSecondPublishButton().click({ timeOut: 6000 });
+    createPostPage.getThirdPublishButton().click({ timeOut: 6000 });
+
+    cy.log("Escenario con video y de acceso solo para miembros.");
+    cy.wait(2500);
+    cy.visit("http://localhost:2368/ghost/");
+    cy.wait(2500);
+    dashboardPage.getPostsMenu().click({ timeOut: 6000 });
+    cy.wait(3500);
+    postPage.getNewPostButton().click({ timeOut: 6000 });
+    createPostPage.getPostTitle().type("A video post for testing with Cypress");
+    createPostPage.getPostContent().click({ timeOut: 6000 });
+    createPostPage.getPostFormat().click({ timeOut: 6000 });
+    cy.wait(2500);
+    createPostPage.getYoutubeFormat().click({ timeOut: 6000 });
+    createPostPage.getPostFormat().click({ timeOut: 6000 });
+    createPostPage.getYoutubeFormat().click({ timeOut: 6000 });
+    cy.wait(2500);
+    createPostPage
+      .getYoutubeUrl()
+      .type("https://www.youtube.com/watch?v=5qap5aO4i9A&ab_channel=LofiGirl");
+    cy.wait(1500);
+    createPostPage.getPostSettings().click({ force: true });
+    cy.wait(1500);
+    createPostPage.getPostAccessDropDown().select("members");
+    createPostPage.getPostSettings().click({ force: true });
+    createPostPage.getFirstPublishButton().click({ timeOut: 6000 });
+    createPostPage.getSecondPublishButton().click({ timeOut: 6000 });
+    createPostPage.getThirdPublishButton().click({ timeOut: 6000 });
     //   /* ==== End ==== */
   });
-  //   it("Crear pagina", function () {
-  //     login();
-  //     cy.get('[href="#/pages/"]').then(($text) => {
-  //       var text = $text.get(0);
-  //       cy.wrap(text).click({ force: true });
-  //     });
-  //     cy.wait(1000);
-  //     cy.get('[href="#/editor/page/"]').then(($text) => {
-  //       var text = $text.get(0);
-  //       cy.wrap(text).type("Hi, now we are just testing. ", { force: true });
-  //     });
-  //     cy.wait(500);
-  //     cy.get("textarea").then(($text) => {
-  //       var text = $text.get(0);
-  //       cy.wrap(text).type("Testing page", { force: true });
-  //     });
-  //     cy.get('[data-placeholder="Begin writing your page..."]').then(($text) => {
-  //       var text = $text.get(0);
-  //       cy.wrap(text).type("This page content is for testing purposes.", {
-  //         force: true,
-  //       });
-  //     });
-  //     cy.wait(1000);
-  //     cy.get("span")
-  //       .contains("Publish")
-  //       .then(($text) => {
-  //         var text = $text.get(0);
-  //         cy.wrap(text).click({ force: true });
-  //       });
-  //     cy.wait(1000);
-  //     cy.get("button > span")
-  //       .contains("Publish")
-  //       .then(($text) => {
-  //         var text = $text.get(0);
-  //         cy.wrap(text).click({ force: true });
-  //       });
-  //     cy.wait(1000);
-  //     cy.get("button")
-  //       .contains("Publish")
-  //       .then(($text) => {
-  //         var text = $text.get(0);
-  //         cy.wrap(text).click({ force: true });
-  //       });
-  //   });
-  //   it("Crear nuevo miembro manualmente", function () {
-  //     login();
-  //     cy.get('[href="#/members/"]').then(($text) => {
-  //       var text = $text.get(0);
-  //       cy.wrap(text).click({ force: true });
-  //     });
-  //     cy.wait(1000);
-  //     cy.get('[href="#/members/new/"]').then(($text) => {
-  //       var text = $text.get(0);
-  //       cy.wrap(text).click({ force: true });
-  //     });
-  //     cy.wait(500);
 
-  //     cy.get('[name="name"]').then(($text) => {
-  //       var text = $text.get(0);
-  //       cy.wrap(text).type("George Tester", { force: true });
-  //     });
-  //     cy.get('[name="email"]').then(($text) => {
-  //       var text = $text.get(0);
-  //       cy.wrap(text).type("testing@gmail.com", {
-  //         force: true,
-  //       });
-  //     });
-  //     cy.get('[name="note"]').then(($text) => {
-  //       var text = $text.get(0);
-  //       cy.wrap(text).type("Some random text", {
-  //         force: true,
-  //       });
-  //     });
-  //     cy.wait(1000);
-  //     cy.get("span")
-  //       .contains("Save")
-  //       .then(($text) => {
-  //         var text = $text.get(0);
-  //         cy.wrap(text).click({ force: true });
-  //       });
-  //   });
-  //   it("Editar post", function () {
-  //     login();
-  //     /* ==== Edit post function ==== */
-  //     cy.get("#ember26").then(($text) => {
-  //       var text = $text.get(0);
-  //       cy.wrap(text).click({ force: true });
-  //     });
-  //     cy.wait(2000);
+  it("Crear pagina", function () {
+    login();
 
-  //     cy.get("span")
-  //       .contains("By")
-  //       .then(($text) => {
-  //         var text = $text.get(getRandomInt(0, $text.length));
-  //         cy.wrap(text).click({ force: true });
-  //       });
+    cy.wait(1000);
+    cy.get('[href="#/editor/page/"]').then(($text) => {
+      var text = $text.get(0);
+      cy.wrap(text).type("Hi, now we are just testing. ", { force: true });
+    });
+    cy.wait(500);
+    cy.get("textarea").then(($text) => {
+      var text = $text.get(0);
+      cy.wrap(text).type("Testing page", { force: true });
+    });
+    cy.get('[data-placeholder="Begin writing your page..."]').then(($text) => {
+      var text = $text.get(0);
+      cy.wrap(text).type("This page content is for testing purposes.", {
+        force: true,
+      });
+    });
+    cy.wait(1000);
+    cy.get("span")
+      .contains("Publish")
+      .then(($text) => {
+        var text = $text.get(0);
+        cy.wrap(text).click({ force: true });
+      });
+    cy.wait(1000);
+    cy.get("button > span")
+      .contains("Publish")
+      .then(($text) => {
+        var text = $text.get(0);
+        cy.wrap(text).click({ force: true });
+      });
+    cy.wait(1000);
+    cy.get("button")
+      .contains("Publish")
+      .then(($text) => {
+        var text = $text.get(0);
+        cy.wrap(text).click({ force: true });
+      });
+  });
 
-  //     cy.wait(3000);
-  //     cy.get("textarea").then(($title) => {
-  //       var title = $title.get(0);
-  //       cy.wrap(title).type("Edited Post ", { force: true });
-  //     });
-  //     cy.get('[data-placeholder="Begin writing your post..."]').then(($text) => {
-  //       var text = $text.get(0);
-  //       cy.wrap(text).type("This content has been edited for testing purposes.", {
-  //         force: true,
-  //       });
-  //     });
-  //     cy.wait(1000);
-  //     cy.get("span")
-  //       .contains("Publish")
-  //       .then(($text) => {
-  //         var text = $text.get(0);
-  //         cy.wrap(text).click({ force: true });
-  //       });
-  //     cy.wait(1000);
-  //     cy.get("button > span")
-  //       .contains("Publish")
-  //       .then(($text) => {
-  //         var text = $text.get(0);
-  //         cy.wrap(text).click({ force: true });
-  //       });
-  //     cy.wait(1000);
-  //     cy.get("button")
-  //       .contains("Publish")
-  //       .then(($text) => {
-  //         var text = $text.get(0);
-  //         cy.wrap(text).click({ force: true });
-  //       });
-  //     /* ==== End ==== */
-  //   });
+  it("Crear nuevo miembro manualmente", function () {
+    login();
+    cy.get('[href="#/members/"]').then(($text) => {
+      var text = $text.get(0);
+      cy.wrap(text).click({ force: true });
+    });
+    cy.wait(1000);
+    cy.get('[href="#/members/new/"]').then(($text) => {
+      var text = $text.get(0);
+      cy.wrap(text).click({ force: true });
+    });
+    cy.wait(500);
+
+    cy.get('[name="name"]').then(($text) => {
+      var text = $text.get(0);
+      cy.wrap(text).type("George Tester", { force: true });
+    });
+    cy.get('[name="email"]').then(($text) => {
+      var text = $text.get(0);
+      cy.wrap(text).type("testing@gmail.com", {
+        force: true,
+      });
+    });
+    cy.get('[name="note"]').then(($text) => {
+      var text = $text.get(0);
+      cy.wrap(text).type("Some random text", {
+        force: true,
+      });
+    });
+    cy.wait(1000);
+    cy.get("span")
+      .contains("Save")
+      .then(($text) => {
+        var text = $text.get(0);
+        cy.wrap(text).click({ force: true });
+      });
+  });
+  it("Editar post", function () {
+    login();
+    /* ==== Edit post function ==== */
+    cy.get("#ember26").then(($text) => {
+      var text = $text.get(0);
+      cy.wrap(text).click({ force: true });
+    });
+    cy.wait(2000);
+
+    cy.get("span")
+      .contains("By")
+      .then(($text) => {
+        var text = $text.get(getRandomInt(0, $text.length));
+        cy.wrap(text).click({ force: true });
+      });
+
+    cy.wait(3000);
+    cy.get("textarea").then(($title) => {
+      var title = $title.get(0);
+      cy.wrap(title).type("Edited Post ", { force: true });
+    });
+    cy.get('[data-placeholder="Begin writing your post..."]').then(($text) => {
+      var text = $text.get(0);
+      cy.wrap(text).type("This content has been edited for testing purposes.", {
+        force: true,
+      });
+    });
+    cy.wait(1000);
+    cy.get("span")
+      .contains("Publish")
+      .then(($text) => {
+        var text = $text.get(0);
+        cy.wrap(text).click({ force: true });
+      });
+    cy.wait(1000);
+    cy.get("button > span")
+      .contains("Publish")
+      .then(($text) => {
+        var text = $text.get(0);
+        cy.wrap(text).click({ force: true });
+      });
+    cy.wait(1000);
+    cy.get("button")
+      .contains("Publish")
+      .then(($text) => {
+        var text = $text.get(0);
+        cy.wrap(text).click({ force: true });
+      });
+    /* ==== End ==== */
+  });
 });
 
 function login() {
--- cypress/support/dashboardPage.js
+++ cypress/support/dashboardPage.js
@@ -1,24 +1,31 @@
 class DashboardPage {
   getPostsMenu() {
-    return cy.get("#ember26");
+    return cy.get('[href="#/posts/"]').then(($text) => {
+      var text = $text.get(0);
+      cy.wrap(text).click({ force: true });
+    });
   }
+
   getDraftsMenu() {
-    return cy.get("#ember46");
+    return cy.get('[href="#/posts/?type=draft"]');
   }
   getScheduledMenu() {
-    return cy.get("#ember47");
+    return cy.get('[href="#/posts/?type=scheduled"]');
   }
   getPublishedMenu() {
-    return cy.get("#ember48");
+    return cy.get('[href="#/posts/?type=published"]');
   }
   getPagesMenu() {
-    return cy.get("#ember28");
+    return cy.get('[href="#/pages/"]').then(($text) => {
+      var text = $text.get(0);
+      cy.wrap(text).click({ force: true });
+    });
   }
   getTagsMenu() {
-    return cy.get("#ember29");
+    return cy.get('[href="#/tags/"]');
   }
   getMembersMenu() {
-    return cy.get("#member30");
+    return cy.get('[href="#/members/"]');
   }
 }
 export default DashboardPage;
